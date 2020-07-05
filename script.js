f4286053521ce0fe1353223be114161316f87816
Apekshabisen <apeksha.bisen@gmail.com>
2020-07-05 23:22:34 +0530

    My Website

---
+++ script.js
@@ -0,0 +1,16 @@
+AOS.init(
+{
+    duration:1600,
+}
+)
+
+$(function () {
+  $(window).on('scroll', function () {
+      if ( $(window).scrollTop() > 10 ) {
+          $('.navbar').addClass('active');
+      } else {
+          $('.navbar').removeClass('active');
+      }
+  });
+});
+
